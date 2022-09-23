dacc8b6fdc8195412295e02917c39968318ec4b8
Emel <kestenemel@gmail.com>
2022-09-23 13:53:29 +0200

    commit

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-const baseUrl = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
-const div = document.querySelector('.div');
-const container = document.querySelector('.container');
-
+const baseUrl = "https://zoo-animal-api.herokuapp.com/animals/rand/10";
+const div = document.querySelector(".imageContainer");
+const container = document.querySelector(".loader");
 function getAnimals() {
     fetch(baseUrl).then(response => response.json()).then(data => {
             console.log(data);
-            let output = '';
+            let output = "";
             data.forEach(animal => {
-                const minidiv = document.createElement('div');
-                minidiv.classList.add('animal');
+                const minidiv = document.createElement("div");
+                minidiv.classList.add("animal");
                 minidiv.innerHTML = `<p>${animal.name}</p> <p>Type: ${animal.animal_type}</p> <p>Habitat: ${animal.habitat}</p> <p>Place: ${animal.geo_range}</p> <p>Lifespan: ${animal.lifespan} years</p> <br><img class="image" src="${animal.image_link}">`;
                 div.appendChild(minidiv);
             });
             if(data){
-                container.classList.add('noshow');
+                container.classList.add("noshow");
             }
         })
         .catch(err => console.log(err));
 }
-
-
-
 function timeoutFunction() {
     setTimeout(function(){
         getAnimals();
     }, 2000);
 }
-
 timeoutFunction()
